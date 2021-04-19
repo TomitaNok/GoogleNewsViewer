1f9f41bb5f848e58df579c9751b218db9bcb64c7
TomitaNok <tomitanok@sohobb.jp>
2021-04-20 00:36:59 +0900

    implemant list page and refactoring

--- swipe/center_test.js
+++
@@ -1,5 +0,0 @@
-var y = 0;
-document.getElementById('dog1').onclick = function() {
-    y += 32;
-    document.getElementById('dog1').style.top = y + "px";
-}
--- swipe/swipe.js
+++ swipe/swipe.js
@@ -1,32 +1,31 @@
 function listen() {
+  const docs = document.getElementsByClassName('center');
+  const doc = Array.from(docs)[0];
+  const { x } = doc.getBoundingClientRect();
 
-    const docs = document.getElementsByClassName('center')
-    const doc = Array.from(docs)[0]
-    const { x } = doc.getBoundingClientRect()
-
-    const FIRST_X_INDEX = x
-    let startX = null
-    doc.addEventListener('mousedown', (e) => {
-        // offset : ページ内での位置
-        const { x } = e
-        startX = x;
-    })
-    document.body.addEventListener('mouseup', (e) => {
-        if (!startX) return
-        startX = null
-        doc.style.left = FIRST_X_INDEX
-    })
-    doc.addEventListener('mousemove', (e) => {
-        if (!startX) return;
-        // x 絶対位置
-        const { x } = e
-        doc.style.left = FIRST_X_INDEX + (x - startX)
-    })
+  const FIRST_X_INDEX = x;
+  let startX = null;
+  doc.addEventListener('mousedown', (e) => {
+    // offset : ページ内での位置
+    const { x } = e;
+    startX = x;
+  });
+  document.body.addEventListener('mouseup', (e) => {
+    if (!startX) return;
+    startX = null;
+    doc.style.left = FIRST_X_INDEX;
+  });
+  doc.addEventListener('mousemove', (e) => {
+    if (!startX) return;
+    // x 絶対位置
+    const { x } = e;
+    doc.style.left = FIRST_X_INDEX + (x - startX);
+  });
 }
 
 // 画像のURLを返す
 async function getImageUrl(word) {
-    return fetch('http://localhost:4000/search/image/' + word)
-        .then((res) => res.json())
-        .then((json) => json.imageUrl);
+  return fetch('http://localhost:4000/search/image/' + encodeURI(word))
+    .then((res) => res.json())
+    .then((json) => json.imageUrl);
 }
